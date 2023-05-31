cd107be88b831b9c5443d64ead6d612836f03f27
mu <cs.21.065@student.uotechnology.edu.iq>
2023-05-31 17:40:25 +0300

    a

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Item.Controllers
+namespace Menu.Controllers
 {
     [ApiController]
     [Route("api/items")]
